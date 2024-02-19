0e5bdb169cae6192a8b9b350c11c9c910672a3aa
andrexleo <andy_61190@hotmail.com>
2024-02-18 19:57:53 -0600

    se elimino bg de footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ const bgImage = {
 };
 const Footer = () => {
   return (
-    <div style={bgImage} className=" text-white">
+    <div className=" text-white">
       <div className="bg-black/40 min-h-[400px]">
         <div className="container grid md:grid-cols-3 pb-20 pt-5">
           {/* company details */}
